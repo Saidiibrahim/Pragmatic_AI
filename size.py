c3db37957fbd903b73f4e89db7e5c29dae58c764
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2022-04-07 20:52:53 +0930

    first commit

---
+++ size.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def main(file):
+    bytes_size = os.path.getsize(file)
+    mb_size = int(bytes_size/1024**2)
+    print(mb_size, "MB")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("""
+        This is a CLI tool that calculates file sizes
+        and it takes a single argument
+        """)
+        sys.exit(0) # So that the program terminates here
+    file = sys.argv[-1]
+    main(file)
