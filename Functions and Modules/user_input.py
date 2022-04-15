15a89dd85be93aa59dbdce6228fa3ea2d8405464
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2022-04-15 10:00:52 +0930

    Finished module and ntbk 😀

--- Functions and Modules/user_input.py
+++ Functions and Modules/user_input.py
@@ -4,23 +4,25 @@ Create a function that given some input (string)
 will output True or False.
 True values in strings are like "1", "Y", "Yes"
 False values are like "0", "N", "N0"
-
-
-function_input = "1"
-true_vals = ["1", "Y", "Yes"]
-false_vals = ["0", "N", "No"]
-if function_input in true_vals:
-    print("True")
-elif function_input in false_vals:
-    print("False")
 """
 
 def str_to_bool(string):
-    true_vals = ["1", "Y", "Yes"]
-    false_vals = ["0", "N", "No"]
+    print(f"{string}")
+    string = string.lower()
+    true_vals = ["1", "y", "yes"]
+    false_vals = ["0", "n", "no"]
     if string in true_vals:
         return True
     elif string in false_vals:
         return False
+    print(f"Do not know what happened. Value {string}")
 
-print(str_to_bool())
+#print(str_to_bool())
+
+# End note: A module in Python is a file.
+# If we define a function in a module, we
+# can import them elsewhere
+# This is ideal for good structuring and organisation
+# and separation of concerns
+# We could have called this file utils or helpers,
+# but helpers for what. Try to be explicit whenever possible
